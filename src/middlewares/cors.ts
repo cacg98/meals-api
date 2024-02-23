1298d76cc08b0975840313554863b2dcf9bfd08e
Carlos Colmenares <ccolmenares@jacidi.com>
2024-02-23 16:06:18 -0400

    fix: cors

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,16 +1,18 @@
-import cors from 'cors'
-import { CustomError } from '../common/customError'
+import cors from "cors";
+import { CustomError } from "../common/customError";
 
-export const corsMiddleware = () => cors({
+export const corsMiddleware = () =>
+  cors({
     origin: (origin, cb) => {
-        const ACCEPTED_ORIGINS = [
-            'http://localhost:4200'
-        ]
-        
-        if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-            return cb(null, true)
-        }
+      const ACCEPTED_ORIGINS = [
+        "http://localhost:4200",
+        "http://localhost:43907",
+      ];
 
-        return new CustomError('Not allowed by CORS', 403)
-    }
-})
+      if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+        return cb(null, true);
+      }
+
+      return cb(new CustomError("Not allowed by CORS", 403));
+    },
+  });
