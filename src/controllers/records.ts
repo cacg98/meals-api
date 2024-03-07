60339e82dabdb621d97eed786232d3ab0e55f576
Carlos Colmenares <ccolmenares@jacidi.com>
2024-03-07 16:39:00 -0400

    feat: records pagination

--- src/controllers/records.ts
+++ src/controllers/records.ts
@@ -1,14 +1,32 @@
-import { Response, NextFunction } from 'express'
-import { tryCatchFn } from '../common/utils'
-import { CustomRequest } from '../common/interfaces/customRequest'
-import * as recordsService from '../services/records'
+import { Response, NextFunction } from "express";
+import { tryCatchFn } from "../common/utils";
+import { CustomRequest } from "../common/interfaces/customRequest";
+import { CustomError } from "../common/customError";
+import * as recordsService from "../services/records";
 
-export const getRecords = tryCatchFn( async (req: CustomRequest, res: Response, next: NextFunction) => {
-    const records = await recordsService.getRecords(req.id)
-    res.send(records)
-})
+export const getRecords = tryCatchFn(
+  async (req: CustomRequest, res: Response, next: NextFunction) => {
+    const { page, size } = req.query;
+    if (!page) throw new CustomError("Missing queryParam: page", 400);
+    if (!size) throw new CustomError("Missing queryParam: size", 400);
+    const result = await recordsService.getRecords(
+      req.id,
+      Number(page),
+      Number(size)
+    );
+    res.send(result);
+  }
+);
 
-export const createOrUpdateRecord = tryCatchFn( async (req: CustomRequest, res: Response, next: NextFunction) => {
-    const records = await recordsService.createOrUpdateRecord(req.id, req.body)
-    res.send(records)
-})
+export const createOrUpdateRecord = tryCatchFn(
+  async (req: CustomRequest, res: Response, next: NextFunction) => {
+    const { size } = req.query;
+    if (!size) throw new CustomError("Missing queryParam: size", 400);
+    const result = await recordsService.createOrUpdateRecord(
+      req.id,
+      req.body,
+      Number(size)
+    );
+    res.send(result);
+  }
+);
--- src/services/records.ts
+++ src/services/records.ts
@@ -1,43 +1,57 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-import { Record } from '../models/record'
-import { IRecord } from '../common/interfaces/record'
+import { Record } from "../models/record";
+import { IRecord } from "../common/interfaces/record";
 
-export async function getRecords(userId: string) {
-    return await Record.find({ user: userId }, { date: 1, ingredients: 1, image: 1 }).sort({ date: -1 }).limit(10)
+export async function getRecords(userId: string, page: number, size: number) {
+  const data = await Record.find(
+    { user: userId },
+    { date: 1, ingredients: 1, image: 1 }
+  )
+    .sort({ date: -1 })
+    .skip(page * size)
+    .limit(size);
+
+  const count = await Record.countDocuments({ user: userId });
+
+  return { data, count };
 }
 
-export async function createOrUpdateRecord(userId: string, record: IRecord) {
-    const sortedIngredients = record.ingredients.sort()
-    const result = await Record.aggregate([
-        {
-            $match: {
-                user: new mongoose.Types.ObjectId(userId)
-            }
+export async function createOrUpdateRecord(
+  userId: string,
+  record: IRecord,
+  size: number
+) {
+  const sortedIngredients = record.ingredients.sort();
+  const result = await Record.aggregate([
+    {
+      $match: {
+        user: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $addFields: {
+        sortedIngredients: {
+          $sortArray: {
+            input: "$ingredients",
+            sortBy: 1,
+          },
         },
-        {
-            $addFields: {
-                sortedIngredients: {
-                    $sortArray: {
-                        input: "$ingredients",
-                        sortBy: 1
-                    }
-                }
-            }
-        },
-        {
-            $match: {
-                sortedIngredients: sortedIngredients
-            }
-        }
-    ])
+      },
+    },
+    {
+      $match: {
+        sortedIngredients: sortedIngredients,
+      },
+    },
+  ]);
 
-    if (result.length) {
-        await Record.updateOne({ _id: result[0]._id }, { date: new Date() })
-    } else {
-        record.user = userId
-        await Record.create(record)
-    }
+  if (result.length) {
+    await Record.updateOne({ _id: result[0]._id }, { date: new Date() });
+  } else {
+    record.user = userId;
+    await Record.create(record);
+  }
 
-    return await Record.find({ user: userId }, { date: 1, ingredients: 1, image: 1 }).sort({ date: -1 }).limit(10)
+  return await getRecords(userId, 0, size);
 }
