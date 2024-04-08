8df4da808584d0606b08f9a11e9685722ccf8128
Carlos Colmenares <ccolmenares@jacidi.com>
2024-04-08 12:36:35 -0400

    feat: update cors

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -7,7 +7,7 @@ export const corsMiddleware = () =>
       const ACCEPTED_ORIGINS = [
         "http://localhost:4200",
         "https://meals-app-urmy.onrender.com",
-        "https://mango-tree-0eb92790f.5.azurestaticapps.net"
+        "https://meals-front.livelybay-8a7bca45.eastus.azurecontainerapps.io"
       ];
 
       if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
